11a88fe38976ec70820b7b8c6ab1168118d673a4
Azeemuddin Aziz <azeemuddinaziz@gmail.com>
2025-07-10 13:38:30 +0530

    remove boilerplate code

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue';
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Hello</h1>
+    <RouterLink to="/signin"><UButton onclick="">Go to Signin Page</UButton></RouterLink>
   </main>
 </template>
