cde8b57790dae70c5389c6e40170776a0e1b56f4
Azeemuddin Aziz <azeemuddinaziz@gmail.com>
2025-07-16 20:32:59 +0530

    Refactor App, About, and Home views for improved structure and clarity

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,16 +1,5 @@
 <template>
-  <div class="about">
+  <div>
     <h1>This is an about page</h1>
-    <UAlert title="Hello!" />
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
